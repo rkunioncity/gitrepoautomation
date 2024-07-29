551f39a16add31b300fd0ce2e08d5ebc6f60ff96
radhe <rk.unioncity4@gmail.com>
2024-07-29 12:11:15 -0700

    added second hoop

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,8 @@
-public class Demo{
-        public static void main(String[] args) {
-                System.out.println("Hello");
-                System.out.println("hoops");
-                System.out.println("hoop2");
-                //comment
-        }
-        }
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("hoops");
+        System.out.println("hoop2");
+        //comment
+    }
+}
